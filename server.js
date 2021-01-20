f2eca1f82ceab07504d3b1172f4fd3cf46ba21ac
Gabriel <gabrielcaiana@hotmail.com>
2021-01-20 20:53:45 -0300

    update api

--- server.js
+++ server.js
@@ -1,81 +1,135 @@
-const fs = require('fs')
-const bodyParser = require('body-parser')
-const jsonServer = require('json-server')
-const jwt = require('jsonwebtoken')
-const cool = require('cool-ascii-faces');
-const express = require('express');
+const fs = require("fs");
+const bodyParser = require("body-parser");
+const jsonServer = require("json-server");
+const jwt = require("jsonwebtoken");
+const cool = require("cool-ascii-faces");
+const express = require("express");
 const PORT = process.env.PORT || 5000;
 
 function ignoreFavicon(req, res, next) {
- if (req.originalUrl.includes('favicon.ico')) {
-   res.status(204).end()
-   return
- }
- next();
+  if (req.originalUrl.includes("favicon.ico")) {
+    res.status(204).end();
+    return;
+  }
+  next();
 }
 
-const server = jsonServer.create()
+const server = jsonServer.create();
 
-const router = jsonServer.router('./db.json')
+const router = sonServer.router("./db.json")
 
-const userdb = JSON.parse(fs.readFileSync('./users.json', 'UTF-8'))
+let userdb = JSON.parse(fs.readFileSync("./users.json", "UTF-8"));
 
-server.use(bodyParser.urlencoded({extended: true}))
-server.use(bodyParser.json())
+server.use(bodyParser.urlencoded({ extended: true }));
+server.use(bodyParser.json());
 
-server.use(ignoreFavicon)
+server.use(ignoreFavicon);
 
 server.use(jsonServer.defaults());
 
-const SECRET_KEY = '123456789'
-const expiresIn = '1h'
+const SECRET_KEY = "123456789";
+const expiresIn = "1h";
 
-// Create a token from a payload 
-function createToken(payload){
- return jwt.sign(payload, SECRET_KEY, {expiresIn})
+// Create a token from a payload
+function createToken(payload) {
+  return jwt.sign(payload, SECRET_KEY, { expiresIn });
 }
 
-// Verify the token 
-function verifyToken(token){
- return  jwt.verify(token, SECRET_KEY, (err, decode) => decode !== undefined ?  decode : err)
+// Verify the token
+function verifyToken(token) {
+  return jwt.verify(token, SECRET_KEY, (err, decode) =>
+    decode !== undefined ? decode : err
+  );
 }
 
 // Check if the user exists in database
-function isAuthenticated({email, password}){
- return userdb.users.findIndex(user => user.email === email && user.password === password) !== -1
+function isAuthenticated({ email, password }) {
+  return (
+    userdb.users.findIndex(
+      (user) => user.email === email && user.password === password
+    ) !== -1
+  );
 }
 
-server.post('/auth/login', (req, res) => {
- const {email, password} = req.body
- if (isAuthenticated({email, password}) === false) {
-   const status = 401
-   const message = 'Incorrect email or password'
-   res.status(status).json({status, message})
-   return
- }
- const access_token = createToken({email, password})
- res.status(200).json({access_token})
-})
+server.post("/auth/register", (req, res) => {
+  console.log(req.body);
+  const { email, password, nome } = req.body;
 
-server.use(/^(?!\/auth).*$/,  (req, res, next) => {
- if (req.headers.authorization === undefined || req.headers.authorization.split(' ')[0] !== 'Bearer') {
-   const status = 401
-   const message = 'Bad authorization header'
-   res.status(status).json({status, message})
-   return
- }
- try {
-    verifyToken(req.headers.authorization.split(' ')[1])
-    next()
- } catch (err) {
-   const status = 401
-   const message = 'Error: access_token is not valid'
-   res.status(status).json({status, message})
- }
-})
+  if (isAuthenticated({ email, password }) === true) {
+    const status = 401;
+    const message = "E-mail já foi utilizado!";
+    res.status(status).json({ status, message });
+    return;
+  }
 
-server.use(router)
+  fs.readFile("./users.json", (err, data) => {
+    if (err) {
+      const status = 401;
+      const message = err;
+      res.status(status).json({ status, message });
+      return;
+    }
 
-express()
-  server.get('/cool', (req, res) => res.send(cool()))
-  server.listen(PORT, () => console.log(`Listening on ${ PORT }`));
+    var data = JSON.parse(data.toString());
+
+    var last_item_id =
+      data.users.length > 0 ? data.users[data.users.length - 1].id : 0;
+
+    data.users.push({ id: last_item_id + 1, email, password, nome });
+    var writeData = fs.writeFile(
+      "./users.json",
+      JSON.stringify(data),
+      (err, result) => {
+        if (err) {
+          const status = 401;
+          const message = err;
+          res.status(status).json({ status, message });
+          return;
+        }
+      }
+    );
+    userdb = data;
+  });
+
+  const access_token = createToken({ email, password });
+  console.log("Access Token:" + access_token);
+  res.status(200).json({ access_token });
+});
+
+server.post("/auth/login", (req, res) => {
+  const { email, password } = req.body;
+  if (isAuthenticated({ email, password }) === false) {
+    const status = 401;
+    const message = "Incorrect email or password";
+    res.status(status).json({ status, message });
+    return;
+  }
+  const access_token = createToken({ email, password });
+  res.status(200).json({ access_token });
+});
+
+server.use(/^(?!\/auth).*$/, (req, res, next) => {
+  if (
+    req.headers.authorization === undefined ||
+    req.headers.authorization.split(" ")[0] !== "Bearer"
+  ) {
+    const status = 401;
+    const message = "Bad authorization header";
+    res.status(status).json({ status, message });
+    return;
+  }
+  try {
+    verifyToken(req.headers.authorization.split(" ")[1]);
+    next();
+  } catch (err) {
+    const status = 401;
+    const message = "Error: access_token is not valid";
+    res.status(status).json({ status, message });
+  }
+});
+
+server.use(router);
+
+express();
+server.get("/cool", (req, res) => res.send(cool()));
+server.listen(PORT, () => console.log(`Listening on ${PORT}`));
